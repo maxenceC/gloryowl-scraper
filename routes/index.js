2b8f7a6ba454db1977920111a4c6604934fdb51a
Maxence Cornet <maxence.cornet20@gmail.com>
2016-07-10 22:34:10 +0200

    add _id + img Url for each comic

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,50 @@
 var express = require('express');
 var scraperjs = require('scraperjs');
 var router = express.Router();
+var scrapRouter = new scraperjs.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.status(200).send({});
 });
 
+
+router.route("https://www.youtube.com/watch/YE7VzlLtp-4", function () {
+    console.log("i'm done");
+});
+
 router.get('/feed', function (req, res, next) {
     res.status(200).send({"test": true});
-    var urls = [];
 
-    scraperjs.StaticScraper.create('http://gloryowlcomix.blogspot.com/')
-        .scrape(function ($) {
-            return $(".separator a").map(function () {
-                return $(this).attr('href');
-            }).get();
-        })
-        .then(function (news) {
-            console.log(news);
-        });
+    scrapeTargetPage('http://gloryowlcomix.blogspot.com');
 
 });
+
+var scrapeTargetPage = function (page) {
+    scraperjs.StaticScraper.create(page)
+        .scrape(function ($) {
+            var nextPage = $(".blog-pager-older-link")[0].attribs.href;
+
+            const comicsLinks = $(".hentry").map(function () {
+                const comicReference = $(this).find('.entry-title a').text();
+                const comicImg = $(this).find('.separator a').attr('href');
+
+                const comic = {
+                    _id: comicReference,
+                    img: comicImg
+                };
+
+                console.log(comic);
+                return this;
+            }).get();
+
+
+            return {comics: comicsLinks, nextPage: nextPage}
+        })
+        .then(function (comics) {
+            //console.log(comics.comics);
+            //scrapeTargetPage(comics.nextPage)
+        });
+};
 
 module.exports = router;
