4f7ed2f31858caf5a1931e3d6e30b6e2321982e5
Maxence Cornet <maxence.cornet20@gmail.com>
2016-07-10 23:35:45 +0200

    Added cron jobs + GET /comics endpoint

--- app.js
+++ app.js
@@ -4,32 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var Allcomics = [];
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/* GET home page. */
+app.get('/comics', function (req, res, next) {
+    res.status(200).send({comics: Allcomics});
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,24 +32,73 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
 });
 
+var CronJob = require('cron').CronJob;
+var scraperjs = require('scraperjs');
+var scrapRouter = new scraperjs.Router();
+
+var scrapeWebsite = new CronJob({
+    cronTime: '00 30 11 * * 1-7',
+    onTick: function () {
+        Allcomics = [];
+        scrapeTargetPage('http://gloryowlcomix.blogspot.com');
+    },
+    start: false,
+    timeZone: 'America/Los_Angeles'
+});
+
+var scrapeTargetPage = function (page) {
+    scraperjs.StaticScraper.create(page)
+        .scrape(function ($) {
+
+            var shouldContinue = true;
+            var nextPage;
+            var comicsLinks = [];
+
+            if ($(".blog-pager-older-link")[0]) {
+                nextPage = $(".blog-pager-older-link")[0].attribs.href;
+                comicsLinks = $(".hentry").map(function () {
+                    const comicReference = $(this).find('.entry-title a').text();
+                    const comicImg = $(this).find('.separator a').attr('href');
+
+                    return {
+                        _id: comicReference,
+                        img: comicImg
+                    };
+
+
+                }).get();
+            } else {
+                shouldContinue = false;
+                comicsLinks = [];
+            }
+
+
+            return {comics: comicsLinks, nextPage: nextPage, shouldContinue: shouldContinue}
+        })
+        .then(function (comics) {
+            Allcomics = Allcomics.concat(comics.comics);
+            if (comics.shouldContinue) {
+                scrapeTargetPage(comics.nextPage)
+            }
+        });
+};
+
+scrapeWebsite.start();
 
 module.exports = app;
+
+
+
+
+
