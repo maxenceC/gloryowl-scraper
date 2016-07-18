2208fa46f4cacb1405b2b0eaa20cd74e807c832d
Maxence Cornet <maxence.cornet20@gmail.com>
2016-07-18 22:03:18 +0200

    enable cors

--- app.js
+++ app.js
@@ -44,6 +44,8 @@ app.use(cookieParser());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
